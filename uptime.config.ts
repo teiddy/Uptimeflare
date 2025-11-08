dcdc3205202f5b244712eb9e3a42e93a3b6c5956
teiddy <132032834+teiddy@users.noreply.github.com>
2025-11-08 17:44:15 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,6 +1,6 @@
 const pageConfig = {
   // Title for your status page
-  title: "hxd 网站检测",
+  title: "hxd Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
     { link: 'https://vwo50.club', label: 'Blog' },
